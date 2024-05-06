5d7be87a2ffc8e5372d9dd4597339328e3ac1b7f
Thabang391 <thabangmoshka@gmail.com>
2024-05-06 18:28:58 +0200

    first commit

---
+++ drizzle.config.js
@@ -0,0 +1,11 @@
+export default {
+    schema: "./utils/schema.js",
+    driver: 'mysql2',
+    dbCredentials: {
+        host: 'srv979.hstgr.io',
+  user: 'u810493557_studentsys',
+  database: 'u810493557_studentsys',
+  password: '&&God24~26',
+        port:'3306'
+    }
+  };
